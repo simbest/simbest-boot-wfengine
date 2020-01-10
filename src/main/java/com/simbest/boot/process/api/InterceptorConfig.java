db456b7794b6f0d0264fbfcea49c006b9dfca622
wodedaodao <liuhuijie900@163.com>
2020-01-10 16:20:10 +0800

    springboot2.0下自定义拦截器处理静态资源
    springboot2.0下自定义拦截器处理静态资源

--- src/main/java/com/simbest/boot/process/api/InterceptorConfig.java
+++ src/main/java/com/simbest/boot/process/api/InterceptorConfig.java
@@ -6,7 +6,9 @@ package com.simbest.boot.process.api;/**
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
 
 /**
  *@ClassName InterceptorConfig
@@ -17,6 +19,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
  **/
 @Configuration
 public class InterceptorConfig extends  WebMvcConfigurationSupport {
+
+    @Override public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").addResourceLocations("classpath:/templates/").resourceChain(false).addResolver(new VersionResourceResolver()
+                .addContentVersionStrategy("/**"));
+        // 放行swagger
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
